7d51b3d83f67d128c08ba6fffa1bf8d6b0fe307d
wojakpepechad <tokenalert7@gmail.com>
2024-06-18 02:31:03 +0200

    fix

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -3534,7 +3534,7 @@ var AccountModal = function (_a) {
     return (React.createElement(Modal, { title: "Your wallet", onDismiss: onDismiss },
         React.createElement(Text, { fontSize: "20px", bold: true, style: { whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", marginBottom: "8px" } }, account),
         React.createElement(Flex, { mb: "32px" },
-            React.createElement(LinkExternal, { small: true, href: "https://scan.pulsechain.com/address/" + account, mr: "16px" }, "View on Explorer"),
+            React.createElement(LinkExternal, { small: true, href: "https://etherscan.io/address/" + account, mr: "16px" }, "View on Explorer"),
             React.createElement(CopyToClipboard, { toCopy: account }, "Copy Address")),
         React.createElement(Flex, { justifyContent: "center" },
             React.createElement(Button, { scale: "sm", variant: "secondary", onClick: function () {
